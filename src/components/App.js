0c1d8a26c1edc15fd102eb4cf950b920bcadc4a3
prog4life <prog4life@users.noreply.github.com>
2018-03-31 19:19:16 +0300

    Replace state creation to index.js

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { hot, setConfig } from 'react-hot-loader';
+
+// import Home from 'containers/Home';
+import WallPostsSearch from 'containers/WallPostsSearch';
+import NotFoundPage from 'components/NotFoundPage';
+
+setConfig({ logLevel: 'error' }); // ['debug', 'log', 'warn', 'error'(default)]
+
+// TODO: replace store creation to index.js and pass it as prop into here
+// and make this component presentational, not container
+
+const App = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        {/* <Route path="/" exact component={Home} /> */}
+        {/* <Route path="/wall-posts" component={WallPostsSearch} /> */}
+        <Route path="/" exact component={WallPostsSearch} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  </Provider>
+);
+
+App.propTypes = {
+  store: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
+// export default hot(module)(App);
+export default process.env.NODE_ENV === 'production' ? App : hot(module)(App);
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,60 @@
 // import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _debounce from 'lodash.debounce';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap-theme.min.css';
 
-import App from 'containers/App';
+import configureStore from 'store/configureStore';
+import { loadState, saveState } from 'utils/localStorage';
+
+import App from 'components/App';
 
 import 'styles/main.css';
 
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const posts = {
+  54525: {
+    timestamp: 1582357458,
+    authorId: 413870329,
+    postId: 54525,
+    text: 'Мне 29 лет симпатичная девушка. Ищу партнёра для интим отношений ' +
+    'только постоянного, за материальную поддержку',
+    link: 'https://vk.com/club75465366?w=wall-75465366_37844%2Fall',
+  },
+  13585: {
+    timestamp: 1323574580,
+    authorId: 247772351,
+    postId: 13585,
+    text: 'Я ищу спонсора на длительные отношения, я без опыта, ' +
+    'полненькая но не сильно, общительная. Пишите в л/с',
+    link: 'https://vk.com/club75465366?w=wall-75465366_37824%2Fall',
+  },
+};
+
+const persistedState = loadState('vk-search-state') || {};
+
+const store = configureStore({
+  ...persistedState,
+  posts,
+});
+
+store.subscribe(() => console.log('State update ', (new Date()).toLocaleTimeString()));
+
+// TODO: wrap in throttle or debounce
+const saveStateDebounced = _debounce(() => {
+  const { auth } = store.getState();
+  const stateToStore = {
+    auth: { ...auth, isRedirecting: false },
+  };
+
+  console.log('save state debounced ', (new Date()).toLocaleTimeString());
+
+  saveState(stateToStore, 'vk-search-state');
+}, 500, { leading: true, trailing: true });
+
+store.subscribe(saveStateDebounced);
+
+ReactDOM.render(<App store={store} />, document.getElementById('app'));
