bf7b34982884bc91899bb9b1518c524cd757cc6d
prog4life <prog4life@users.noreply.github.com>
2018-06-11 21:45:11 +0300

    Change styling of search result component

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -8,51 +8,89 @@ function SearchResult({ result, number }) {
     timestamp, authorId, id, text, link,
   } = result;
 
-  const resultNumber = `[${number}]`;
-  const linkText = 'Link to post at wall';
+  // const resultNumber = `[${number}]`;
+  const resultNumber = `${number}`;
+  const linkText = 'Open at wall';
   const linkToUserPage = `https://vk.com/id${authorId}`;
   const idLabel = `[post id: ${id}]`;
   const authorIdLabel = `[id${authorId}]`;
-  const postDate = new Date(timestamp * 1000);
+  // const postDate = new Date(timestamp * 1000);
+  const postDate = new Date(1487830000000);
+  // postDate.setDayOfWeek(5);
+  postDate.setMonth(11);
+  postDate.setHours(4);
+  postDate.setMinutes(8);
+  // postDate.setYear(2015);
   const dayOfMonth = postDate.getDate();
-  const month = postDate.getMonth() + 1;
+  const incrMonth = postDate.getMonth() + 1;
+  const monthMap = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+  };
+  // const month = incrMonth < 10 ? `0${incrMonth}` : incrMonth;
+  const month = monthMap[incrMonth];
   const year = postDate.getFullYear();
-  const hours = postDate.getHours();
-  const minutes = postDate.getMinutes();
+  const rawHours = postDate.getHours();
+  const hours = rawHours < 10 ? `0${rawHours}` : rawHours;
+  const rawMinutes = postDate.getMinutes();
+  const minutes = rawMinutes < 10 ? `0${rawMinutes}` : rawMinutes;
   // const timestampString = postDate.toLocaleString('en-GB').slice(0, -3);
 
   return (
     <ListGroupItem>
-      <span>
-        {resultNumber}
+      <div className="search-result__title">
+        <span style={{
+          marginRight: '8px',
+          padding: '0 4px',
+          backgroundColor: '#dedede',
+          textAlign: 'center',
+          fontWeight: '600',
+        }}
+        >
+          {resultNumber}
+          {/* {' '} */}
+        </span>
+        <span className="post-timestamp">
+          {/* {moment.unix(timestamp).format('D MMM YYYY  H:mm')} */}
+          {`${dayOfMonth} ${month} ${year}`}
+          {/* {timestampString} */}
+        </span>
         {' '}
-      </span>
-      <span className="post-timestamp">
-        {/* {moment.unix(timestamp).format('D MMM YYYY  H:mm')} */}
-        {`${dayOfMonth}-${month}-${year}`}
-        {/* {timestampString} */}
-      </span>
-      {' '}
-      <span className="post-timestamp">
-        {`${hours}:${minutes}`}
-      </span>
-      <span>
-        {' '}
-        {idLabel}
-      </span>
-      <span>
-        {' '}
-        <a href={linkToUserPage} target="_blank">
-          {authorIdLabel}
-        </a>
-      </span>
-      <p>
-        {text}
-        {' '}
-        <a href={link} target="_blank">
-          {linkText}
-        </a>
-      </p>
+        <span className="post-timestamp">
+          {`${hours}:${minutes}`}
+        </span>
+        <span>
+          {' '}
+          {idLabel}
+        </span>
+        <span>
+          {' '}
+          <a href={linkToUserPage} target="_blank">
+            {authorIdLabel}
+          </a>
+        </span>
+      </div>
+      <div className="search-result__content">
+        <p>
+          {text}
+          {' '}
+        </p>
+        <div>
+          <a href={link} target="_blank">
+            {linkText}
+          </a>
+        </div>
+      </div>
     </ListGroupItem>
   );
 }
