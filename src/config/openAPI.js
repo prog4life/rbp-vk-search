d3e9eabb6f961b2ef3c24b84186d85da9ae8e738
prog4life <prog4life@users.noreply.github.com>
2018-09-09 19:02:51 +0300

    Modify vk open api init script loading

--- src/config/openAPI.js
+++ src/config/openAPI.js
@@ -1,37 +1,38 @@
 // for sync initialization, openapi.js script in head must be loaded in sync way
-// VK.init({ // eslint-disable-line no-undef
+// VK.init({
 //   apiId: 5931563, // TODO: inject from env var or common app config later
 //   onlyWidgets: false,
 // });
 
-// for async initialization
-// window.vkAsyncInit = () => {
-//   VK.init({ // eslint-disable-line no-undef
-//     apiId: 5931563, // TODO: inject from env var or common app config later
-//     onlyWidgets: false,
-//   });
-//   console.log('VK.init was called');
-// };
-
 const initPromise = new Promise((resolve) => {
+  // for async initialization
   window.vkAsyncInit = () => {
     VK.init({
       apiId: 5931563, // TODO: inject from env var or common app config later
       onlyWidgets: false,
     });
-    // console.log('VK.init was called');
+    // console.log('+++ VK.init was called +++');
     resolve();
   };
 });
 
-// at this moment next script is loaded at page <head> in async way
-// setTimeout(() => {
-//   const el = document.createElement('script');
-//   el.type = 'text/javascript';
-//   el.src = 'https://vk.com/js/api/openapi.js?159';
-//   el.async = true;
-//   document.getElementById('vk_api_transport').appendChild(el);
-//   console.log('Append VK init script');
-// }, 0);
+// NOTE: there was a bug with sript tag (with async attribute), that was added
+// to head, probably due to caching
+setTimeout(() => {
+  const el = document.createElement('script');
+  el.type = 'text/javascript';
+  el.src = 'https://vk.com/js/api/openapi.js?159';
+  el.async = true;
+  // TEMP:
+  el.onload = () => {
+    console.log('::: Open API script "load" :::');
+  };
+  el.onloadstart = () => {
+    console.log('::: Open API script "loadstart" :::');
+  };
+  // --- END ---
+  document.getElementById('vk_api_transport').appendChild(el);
+  console.log('::: Open API script is appended :::');
+}, 0);
 
 export default initPromise;
--- src/constants/ui.js
+++ src/constants/ui.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const AUTH_OFFER_DELAY = 3000;
+export const AUTH_OFFER_DELAY = 5000;
